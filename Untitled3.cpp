e538995f479445020d11b477302e087939e5d315
ahad raza <83767844+ahadraza155@users.noreply.github.com>
2021-06-18 16:24:10 +0500

    conditions statements in class and object(std):

--- Untitled3.cpp
+++ Untitled3.cpp
@@ -4,17 +4,22 @@ class ahadraza
 {
 	public:
 	int x;
-	void my_function(int a)
+	int y;
+	void my_function()
 	{
-		x=a;
-		cout<<"the square of the value of a:"<<(a*a)<<endl;
-		cout<<"the cube of the value of b:"<<(a*a*a)<<endl;
+		cin>>x;
+		cin>>y;
+		if(x==y)
+		cout<<"the square of the value of a:"<<(x*x)<<endl;
+		else
+		cout<<"the cube of the value of b:"<<(y*y*y)<<endl;
 	}
 };
 int main()
 {
 	ahadraza d1;
-	d1.my_function(5);
+	d1.my_function();
 	d1.x;
+	d1.y;
 	return(0);
 }
