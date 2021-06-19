9fddcd8353094aca4e9902ffc5733edf933952f5
ahad raza <83767844+ahadraza155@users.noreply.github.com>
2021-06-19 13:26:00 +0500

    DATA HIDING /ENCAPSULATIONS
    PREPARATIONS OF BOARDS EXAMS:

---
+++ Untitled1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class ahadraza
+{
+	public:
+	{
+		int x;
+		void function_main()	
+	}
+	{
+		cin>>x;
+		for(x=0;x<=15;x++);
+	}
+};
+int main()
+{
+	ahadraza d1;
+	d1.function_main();
+	d1.x;
+	return(0);
+}
